5e98f936cd9bba6eee66b370b2f2065aca4dca7e
Zain Javed <zjaved.bese17seecs@seecs.edu.pk>
2023-11-26 10:18:49 +0500

    CORS Missing Allow Origin error fix

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	_ "github.com/MrNi8mare/word-count-bee/routers"
-
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 )
 
 func main() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"http://localhost:89"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
+	}))
 	beego.Run()
 }
