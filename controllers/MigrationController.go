5ce2819291e4b55f817961cdb79b24b6ea27aa1a
Zain Javed <zjaved.bese17seecs@seecs.edu.pk>
2023-11-30 19:32:04 +0500

    add db key

--- controllers/MigrationController.go
+++ controllers/MigrationController.go
@@ -10,7 +10,15 @@ type MigrationController struct {
 	beego.Controller
 }
 
-func (c *MigrationController) Get() {
+func (c *MigrationController) Post() {
+
+	dbKey := c.Ctx.Input.Header("Key")
+
+	envDBKey := utils.GoDotEnvVariable("DB_ACCESS_KEY")
+
+	if dbKey != envDBKey {
+		utils.CreateErrorResponse(&c.Controller, 422, "Invalid key")
+	}
 
 	db := utils.ConnectDB()
 	db.AutoMigrate(&models.User{})
